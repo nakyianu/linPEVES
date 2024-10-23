370a6f92f2f1697be1df6d862123d5b7f04ef276
clean-hands <34384401+Clean-Hands@users.noreply.github.com>
2024-10-23 17:24:29 +0000

    systemctl-bin-scan.sh almost done

--- scans/systemctl-bin-scan.sh
+++ scans/systemctl-bin-scan.sh
@@ -2,32 +2,6 @@
 
 exploit=true
 
-writable=''
-
-# running this command will define a PATH variable
-# containing directories where systemctl binaries are stored,
-# separated by colons
-eval $(systemctl show-environment) 
-
-# loop through each directory in the PATH variable
-for dir in ${PATH//:/ }; do
-	# if we have write access to the directory,
-	# loop through each file within the directory
-	# keeping track of writable files
-	# TODO: check if this user owns anything, if so give write perms
-	user=$(stat -c "%U" $dir)
-
-	if [[ -w "$dir" ]]; then
-		cd "$dir"
-		for file in *; do
-			if [[ -f "$file" ]] && [[ -w "$file" ]]; then
-				writable+=${dir}/${file}$'\n'
-			fi
-		done
-	fi
-done
-
-echo Writable: $writable
 
 # get locations of the .service files
 dirs=$(systemctl show | grep UnitPath | sed 's/.*=//g')
@@ -45,7 +19,7 @@ for dir in $dirs; do
 			# if the file's extension is '.service', then add it to the list
                         if [[ "${file: -8}" == ".service" ]]; then
                                 services+=${dir}/${file}$'\n'
-				execs+=$(cat ${file} 2>/dev/null | grep -oP "^Exec(?:Start|Stop)=[-+@!\s]*\K(.*)$")$'\n' 
+				execs+=$(cat ${file} 2>/dev/null | grep -oP "^Exec(?:Start|Stop)=[-+@!\s]*\K/(.*)$")$'\n' 
                         fi
                 done
 	fi
@@ -54,17 +28,56 @@ done
 # remove blank lines
 execs=$(echo "${execs}" | sed '/^\s*$/d' | awk '{ print $1 }' | sort | uniq)
 
-echo "${execs}"
+# echo "${execs}"
 
-#while read service; do
-#	echo $service
-#done <<< "$execs"
+writable=''
 
-#for service in ${execs}; do
-#	echo $service
-#done
-#if [[ "$exploit" = true ]] then
-	# echo "modifying ${writable}"
-	# TODO: get username to give user sudo perms
-	# echo usermod -aG sudo $user > $writable
-#fi
+for bin in ${execs}; do
+	user=$(stat -c "%U" $bin)
+	if [[ -w ${bin} ]]; then
+		writable+=$bin$'\n'
+	elif [[ $user = $(whoami) ]] && [[ "$exploit" = true ]]; then
+		chmod u+rw ${bin}
+		writable+=$bin$'\n'
+	fi
+done
+
+
+# running this command will define a PATH variable
+# containing directories where systemctl binaries are stored,
+# separated by colons
+eval $(systemctl show-environment) 
+
+writable_dirs=''
+
+# loop through each directory in the PATH variable
+for dir in ${PATH//:/ }; do
+	# if we have write access to the directory,
+	# loop through each file within the directory
+	# keeping track of writable files
+
+	user=$(stat -c "%U" ${dir})
+	if [[ -w "$dir" ]]; then
+                writable_dirs+=$dir$'\n'
+	elif [[ $user = $(whoami) ]] && [[ "$exploit" = true ]]; then
+                chmod u+rw ${dir}
+                writable_dirs+=$dir$'\n'
+        fi
+done
+
+for dir in $writable_dirs; do
+	cd "$dir"
+	for file in *; do
+		user=$(stat -c "%U" ${file})
+		if [[ -f "$file" ]]; then
+			if [[ -w "$file" ]]; then
+				writable+=${dir}/${file}$'\n'
+			elif [[ $user = $(whoami) ]] && [[ "$exploit" = true ]]; then
+                		chmod u+rw ${file}
+                		writable+=${dir}/${file}$'\n'
+			fi
+		fi
+	done
+done
+
+echo Writable: $writable
