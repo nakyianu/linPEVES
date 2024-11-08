ccc17d02f26c33e7f6315d130ceed4049189c46f
essyi221085 <165684224+essyi221085@users.noreply.github.com>
2024-11-07 20:55:26 -0600

    Update sudo-scan.sh

--- scans/sudo-scan.sh
+++ scans/sudo-scan.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
+EXPLOIT=1
+EXPLOITABLE=0
 
-sudo_ver=$(sudo -V | grep "Sudo ver" | awk '{print $3; exit}' | awk -F '.' '{print $2}') #$ means execute the following 
+sudo_ver=$(sudo -V | grep "Sudo ver" | awk '{print $3; exit}' | awk -F '.' '{print $2}') # $ means execute the following 
                                            # () means its a command, dont use with variable
-if [[ $sudo_ver -eq 8 ]]; then echo vulnerable; else echo not vulnerable; fi
-
-#research how to downgrade sudo ver in aws
+if [[ $sudo_ver -eq 8 ]]; 
+    then EXPLOITABLE=1; 
+    else EXPLOITABLE=0; 
+fi
