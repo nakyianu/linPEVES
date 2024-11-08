9ed962499cef39ec5327921b1b951099f84fb45e
essyi221085 <165684224+essyi221085@users.noreply.github.com>
2024-11-07 20:56:13 -0600

    Update readable-shadow-scan.sh

--- scans/readable-shadow-scan.sh
+++ scans/readable-shadow-scan.sh
@@ -1,6 +1,15 @@
 #! bin bash
 
+EXPLOIT=1       # Determines whether user wants to exploit scan (if possible)
+EXPLOITABLE=0   # Determines whether a scan is possible 
+
+
 if [[ -r /etc/shadow ]];
-    then echo you can read the shadow file!!;
-    else echo shadow inaccessible;
+    then EXPLOITABLE=1;     # Exploit exists within system
+    else EXPLOITABLE=0;     # Exploit does not exist within system
 fi
+
+if [[ "$EXPLOIT" = 1 ]] && [[ "$EXPLOITABLE" = 1 ]]; 
+    then /bin/bash exploits/readable-shadow-exploit.sh
+fi
+
